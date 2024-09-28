dcc0f4c29f79c1595b2679634f3be6cac398c82c
Zefanja Jobse <zefanjaj@gmail.com>
2024-09-28 18:32:30 +0200

    add .sh and .bat file

---
+++ start.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET DATABASE_URL=postgres://postgres:pass@localhost/postgres
+SET SERVER_ID=75a22782-850d-11eb-b244-0a58a9feac02
+SET KEEPALIVE_PORT=3030
+
+gather_playsession.exe
---
+++ start.sh
@@ -0,0 +1,4 @@
+export DATABASE_URL=postgres://postgres:pass@localhost/postgres
+export SERVER_ID=75a22782-850d-11eb-b244-0a58a9feac02
+export KEEPALIVE_PORT=3030
+./gather_playsession
